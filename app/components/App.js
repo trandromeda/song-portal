b2c102f89eb7cb83ed577c7454de948f2bedcbc7
Andy Tran <transient.tran@gmail.com>
2017-07-05 12:09:01 -0400

    moved code from index to app.js

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Song Portal</h1>
+      </div>
+      )
+  }
+}
+
+module.exports = App;
