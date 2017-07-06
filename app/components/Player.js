efdc8187604d22d06bf6d742b0c773cf7af6a23d
Andy Tran <transient.tran@gmail.com>
2017-07-05 22:21:12 -0400

    installed spotify npm package for the web player, next will need to make it modifiable

--- app/components/Player.js
+++ app/components/Player.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import SpotifyPlayer from 'react-spotify-player';
 
 class Player extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      currentSong: null
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+
   render() {
+    // size may also be a plain string using the presets 'large' or 'compact' 
+    const size = 'compact';
+    // {
+    //   width: '100%',
+    //   height: 200,
+    // };
+    const view = 'list'; // or 'coverart' 
+    const theme = 'black'; // or 'white' 
     return (
-      <div className="player-box">
-      <button className="play">Play</button>
+      <div className="player">
+        <SpotifyPlayer
+          uri="spotify:track:6VDfg9kdGhEb4McPwJpp5t"
+          size={size}
+          view={view}
+          theme={theme} />
       </div>
       )
   }
