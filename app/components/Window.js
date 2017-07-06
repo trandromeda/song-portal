002715c391506d3192032da503b576def8d145b6
Andy Tran <transient.tran@gmail.com>
2017-07-06 12:37:59 -0400

    now pulling from the Unsplash API successfully

--- app/components/Window.js
+++ app/components/Window.js
@@ -2,38 +2,66 @@ import React from 'react';
 import Player from './Player';
 import Unsplash, { toJson } from 'unsplash-js';
 
+function Credit {
+  
+}
+
 class Window extends React.Component {
   constructor(props) {
     super();
     this.state = {
       currentPhoto: null,
-      currentPhotographer: ''
+      currentPhotographer: '',
+      currentUsername: ''
     }
   }
 
-  render() {
+  componentDidMount() {
     const unsplash = new Unsplash({
-      applicationId: "0f54ed67488c83cfd7020132c0c66f39439a724452e7d1b61cb22cb1802e4bce",
-      secret: "088f1465e458cb90818a9b8fb3ebe9d0365a2c271ab008874a7ec1ad39eb64f5",
-      callbackUrl: "urn:ietf:wg:oauth:2.0:oob"
+      applicationId: UNSPLASH_APPID,
+      secret: UNSPLASH_SECRET,
+      callbackUrl: UNSPLASH_CALLBACK
     });
 
     unsplash.photos.getRandomPhoto({ query: "water" })
       .then(toJson)
       .then(json => {
-        console.log(json)
-        this.setState(() =>
-            currentPhoto: json.urls.regular
-            // currentPhotographer: json
+        this.setState(
+          {
+            currentPhoto: json.urls.full,
+            currentPhotographer: json.user.first_name + ' ' + json.user.last_name,
+            currentUsername: json.user.username
+          }
         )
-    });
+    });    
+  }
+
+  render() {
+    let divStyle;
+    {!this.state.currentPhoto ? 
+      divStyle = {
+        backgroundColor: '#000000',
+        height: '100vh'
+      }
+      : 
+      divStyle = {
+        background: 'transparent url(' + this.state.currentPhoto + ') no-repeat 50% 50%',
+        height: '100vh'
+      }
+    }
 
     return (
-      <div className="window">
-        <img src={this.state.currentPhoto}/>
+      <div 
+        className="window"
+        style = {divStyle}>
+
         <h1 className="title">SongPortal</h1>
+
         <Player />
-        <p className="credit">Photo by {this.state.currentPhotographer} / Unsplash</p>
+        <p className="credit">Photo by 
+        <a href={"https://unsplash.com/" + "@" + this.state.currentUsername + "?utm_source=songportal&utm_medium=referral&utm_campaign=api-credit"}>{this.state.currentPhotographer}</a> / 
+        <a href={"https://unsplash.com/?utm_source=songportal&utm_medium=referral&utm_campaign=api-credit"}>Unsplash</a></p>
+
       </div>
     )
   }
